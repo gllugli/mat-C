7e6e2f9bac5b2626aaa55cd2f2af58e75d0181be
Gabriel Larsão Lugli <146869089+gllugli@users.noreply.github.com>
2024-05-24 13:55:33 -0300

    Update ex_2_7_mat_tc_22400359_gabriel_larsao.c

--- ex_2_7_mat_tc_22400359_gabriel_larsao.c
+++ ex_2_7_mat_tc_22400359_gabriel_larsao.c
@@ -1,3 +1,8 @@
+/*No programa anterior, nós percorremos a matriz linha por linha, ou seja, 
+fixamos o valor da linha e variamos a coluna. Agora vamos refazê-lo percorrendo 
+a matriz coluna por coluna, ou seja, fixamos o valor da coluna e variamos a linha. 
+No final mostre os valores armazenados na matriz.*/
+
 #include <stdio.h>
 #include <locale.h>
 
@@ -9,7 +14,7 @@ int main() {
 	
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 3; j++) {
-			printf("\nDigite %dº número da %dº coluna da matriz: ", j+1, i+1);
+			printf("\nDigite %dº número da %dª coluna da matriz: ", j+1, i+1);
 			mat[j][i] = atoi(gets(aux));
 		}
 		printf("\n");
@@ -17,7 +22,7 @@ int main() {
 	
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 3; j++) {
-			printf("\n%dº número da %dº coluna da matriz: %d", j+1, i+1, mat[j][i]);
+			printf("\n%dº número da %dª coluna da matriz: %d", j+1, i+1, mat[j][i]);
 		}
 		printf("\n");
 	}
